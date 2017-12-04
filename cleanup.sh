f9c51587f96fca0b918e5cb69b0717be67699e76
Justin Finger <jstinf@vt.edu>
2017-12-03 21:09:42 -0500

    Added removing .csv files to the cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf aclocal.m4 config.* configure Makefile Makefile.in depcomp install-sh missing stamp-h1 autom4te.cache/ compile
+rm -rf aclocal.m4 config.* configure Makefile *.csv Makefile.in depcomp install-sh missing stamp-h1 autom4te.cache/ compile
